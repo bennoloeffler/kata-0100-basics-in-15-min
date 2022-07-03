483a6879b347ea4c1b3389ff9cdd37712e590ffc
bennoloeffler <benno.loeffler@gmx.de>
2022-07-03 12:33:02 +0200

    removed empty tests

--- test/kata_0100_basics_in_15_min/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns kata-0100-basics-in-15-min.core-test
-  (:require [clojure.test :refer :all]
-            [kata-0100-basics-in-15-min.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
